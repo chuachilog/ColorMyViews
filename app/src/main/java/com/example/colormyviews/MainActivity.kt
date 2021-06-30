3b2a46a4fb21e6bbe607586fef6b8ada8fb8e775
chuachilog <chuachilog@yahoo.com>
2021-06-30 13:12:16 +0800

    Another commit

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun makeColored(view: View){
+
+        when (view.id) {
+
+            // Boxes using Color class colors for the background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+
+
+            // Boxes using custom colors for background
+            R.id.red_button -> findViewById<View>(R.id.box_three_text).setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> findViewById<View>(R.id.box_four_text).setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> findViewById<View>(R.id.box_five_text).setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+//            R.id.box_one_text -> view.setBackgroundResource(R.drawable.image_1)
+//            R.id.box_two_text -> view.setBackgroundResource(R.drawable.image_2)
+//            R.id.box_three_text -> view.setBackgroundResource(R.drawable.image_3)
+//            R.id.box_four_text -> view.setBackgroundResource(R.drawable.image_4)
+//            R.id.box_five_text -> view.setBackgroundResource(R.drawable.image_5)
+//            else -> view.setBackgroundResource(R.drawable.image_root)
+
+        }
+    }
+
+    private fun setListeners() {
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
+        val clickableView: List<View> = listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText,rootConstraintLayout,redButton,greenButton,yellowButton)
+
+        for (item in clickableView) {
+            item.setOnClickListener { makeColored(it) }
+        }
+
     }
 }
